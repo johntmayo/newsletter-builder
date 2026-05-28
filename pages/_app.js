1040d880a0d0e5c1d9e18bee0c24510633922bf8
johntmayo <johntmayo@gmail.com>
2026-05-28 12:48:57 -0700

    ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,48 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
 import "../styles/globals.css";
 
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!GA_MEASUREMENT_ID || typeof window.gtag !== "function") return undefined;
+
+    const handleRouteChange = (url) => {
+      window.gtag("config", GA_MEASUREMENT_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
+
+  return (
+    <>
+      {GA_MEASUREMENT_ID ? (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              window.gtag = gtag;
+              gtag('js', new Date());
+              gtag('config', '${GA_MEASUREMENT_ID}', {
+                page_path: window.location.pathname,
+              });
+            `}
+          </Script>
+        </>
+      ) : null}
+      <Component {...pageProps} />
+    </>
+  );
 }
